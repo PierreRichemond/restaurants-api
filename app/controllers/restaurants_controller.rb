c3e230b5f534858e2be150dd0408d1b9ac745eb0
= <pierreambroise@hotmail.fr>
2022-02-17 14:31:38 +0900

    added crud on the restaurant so anybody can add, and remove his own restaurants from the API

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,23 +1,54 @@
 class RestaurantsController < ApplicationController
-  skip_before_action :authenticate_user!, only: [ :home ]
+  skip_before_action :authenticate_user!, only: [ :index ]
+  before_action :set_restaurant, only: [:show, :edit, :update, :destroy]
 
   def index
-    @restaurant = Restaurant.all
+    @restaurants = Restaurant.all
   end
 
   def new
+    @restaurant = Restaurant.new
   end
 
   def create
+    @restaurant = current_user.restaurants.new(restaurant_params)
+    if @restaurant.save
+      flash[:notice] = 'Restaurant has been added to the API'
+      redirect_to [current_user, @restaurant]
+    else
+      flash.now[:alert] = 'Restaurant has not been created'
+      render :new
+    end
   end
 
   def show
   end
 
-  def update
+  def edit
+  end
 
+  def update
+    if @restaurant.update(restaurant_params)
+      flash[:notice] = 'Restaurant has been updated to the API'
+      redirect_to [current_user, @restaurant]
+    else
+      flash.now[:alert] = 'restaurant has not been updated'
+      render :edit
+    end
   end
 
   def destroy
+    @restaurant.destroy
+    flash[:notice] = 'Restaurant has been Removed from the Api'
+    redirect_to user_restaurants_path(current_user)
+  end
+  private
+
+  def set_restaurant
+    @restaurant = current_user.restaurants.find(params[:id])
+  end
+
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :address, :user_id)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'restaurants#index'
-  resources :restaurants do
-    resources :comments
+
+  resources :users do
+      resources :restaurants, except: [:index] do
+          resources :comments
+      end
   end
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   namespace :api, defaults: { format: :json } do
